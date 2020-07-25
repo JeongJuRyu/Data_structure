1e9e157374801543e570e883ff93f7a70bbac6f0
jeongjuryu <totw2018@naver.com>
2020-07-25 09:11:20 +0900

    modify

--- 4_Expression_tree/expression_tree.c
+++ 4_Expression_tree/expression_tree.c
@@ -19,32 +19,44 @@ typedef struct
 	NODE	*root;
 } TREE;
 
+typedef struct
+{
+	NODE* element[MAX_STACK_SIZE];
+	int top;
+}STACK;
 ////////////////////////////////////////////////////////////////////////////////
 // Prototype declarations
 
 /* Allocates dynamic memory for a tree head node and returns its address to caller
-	return	head node pointer
-			NULL if overflow
+return	head node pointer
+NULL if overflow
 */
 TREE *createTree(void) {
-	NODE* root = (NODE*)malloc(sizeof(NODE));
-	root->left = NULL;
-	root->right = NULL;
-	return root;
+	TREE* pTree = (TREE*)malloic(sizeof(TREE));
+	if (pTree == NULL) return NULL;
+	pTree->root = (NODE*)malloc(sizeof(NODE));
+	pTree->root->left = NULL;
+	pTree->root->right = NULL;
+	return pTree;
 }
 
 /* Deletes all data in tree and recycles memory
 */
-void destroyTree( TREE *pTree);
+void destroyTree(TREE *pTree);
 
-static void _destroy( NODE *root);
+static void _destroy(NODE *root) {
+	if (root == NULL) return;
+	_destroy(root->left);
+	_destroy(root->right);
+	free(root);
+}
 
 /*  Allocates dynamic memory for a node and returns its address to caller
-	return	node pointer
-			NULL if overflow
+return	node pointer
+NULL if overflow
 */
 static NODE *_makeNode(char ch) {
-	(NODE*)eNode = (NODE*)malloc(sizeof(NODE));
+	NODE* eNode = (NODE*)malloc(sizeof(NODE));
 	eNode->left = NULL;
 	eNode->right = NULL;
 	eNode->data = ch;
@@ -52,106 +64,137 @@ static NODE *_makeNode(char ch) {
 }
 
 /* converts postfix expression to binary tree
-	return	1 success
-			0 invalid postfix expression
+return	1 success
+0 invalid postfix expression
 */
 int postfix2tree(char *expr, TREE *pTree) {
-	
+	STACK stack;
+	stack.top = -1;
+	int index = 0;
+	while (expr[index] != '\n') {
+		if (index == 0) {
+			if (!isdigit(expr[index])) return 0; //처음부터 연산자가 나오면 바로 invalid
+			stack.element[++stack.top] = _makeNode(expr[index]);
+			continue;
+		}
+		NODE* tempNode = _makeNode(expr[index]);
+		if (isdigit(expr[index])) { //숫자인 경우 push
+			stack.element[++stack.top] = tempNode;
+		}
+		else { // 연산자인 경우, invalid 체크 하고 valid한 경우면 트리 만듦
+			if (stack.top < 1) { //pop을 2번 해야하는데 충분하지 않다면
+				if (stack.top == 0) free(expr[index]);
+				return 0;
+			}
+			tempNode->right = stack.element[stack.top--];
+			tempNode->left = stack.element[stack.top--];
+			stack.element[++stack.top] = tempNode;
+		}
+		index++;
+	}
+	if (stack.top != 0) {
+		while (stack.top > 0) {
+			free(stack.element[stack.top--]);
+		}
+		return 0;
+	}
+	pTree->root = stack.element[0];
+	return 1;
 }
 
 /* Print node in tree using inorder traversal
 */
-void traverseTree( TREE *pTree);
+void traverseTree(TREE *pTree);
 
 /* internal traversal function
-	an implementation of ALGORITHM 6-6
+an implementation of ALGORITHM 6-6
 */
-static void _traverse( NODE *root);
+static void _traverse(NODE *root);
 
 /* Print tree using inorder right-to-left traversal
 */
-void printTree( TREE *pTree);
+void printTree(TREE *pTree);
 
 /* internal traversal function
 */
-static void _infix_print( NODE *root, int level);
+static void _infix_print(NODE *root, int level);
 
 /* evaluate postfix expression
-	return	value of expression
+return	value of expression
 */
-float evalPostfix( char *expr);
+float evalPostfix(char *expr);
 
 ////////////////////////////////////////////////////////////////////////////////
-void destroyTree( TREE *pTree)
+void destroyTree(TREE *pTree)
 {
 	if (pTree)
 	{
 		//_destroy( pTree->root);
 	}
-		
-	free( pTree);
+
+	free(pTree);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void printTree( TREE *pTree)
+void printTree(TREE *pTree)
 {
-	//_infix_print(pTree->root, 0);
-	
+	infix_print(pTree->root, 0);
+
 	return;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void traverseTree( TREE *pTree)
+void traverseTree(TREE *pTree)
 {
 	//_traverse(pTree->root);
-	
+
 	return;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-int main( int argc, char **argv)
+int main(int argc, char **argv)
 {
 	TREE *tree;
 	char expr[1024];
-	
-	fprintf( stdout, "\nInput an expression (postfix): ");
-	
-	while (fscanf( stdin, "%s", expr) == 1)
+
+	fprintf(stdout, "\nInput an expression (postfix): ");
+
+	while (fscanf(stdin, "%s", expr) == 1)
 	{
 		tree = createTree();
-		
+
 		if (!tree)
 		{
-			printf( "Cannot create tree\n");
+			printf("Cannot create tree\n");
 			return 100;
 		}
-		
+
 		// postfix expression -> expression tree
-		//int ret = postfix2tree( expr, tree);
-		//if (!ret)
+		int ret = postfix2tree( expr, tree);
+		if (!ret)
 		{
-		//	fprintf( stdout, "invalid expression!\n");
-		//	destroyTree( tree);
-		//	fprintf( stdout, "\nInput an expression (postfix): ");
-		//	continue;
+			fprintf( stdout, "invalid expression!\n");
+			destroyTree( tree);
+			fprintf( stdout, "\nInput an expression (postfix): ");
+			continue;
 		}
-		
+
 		// expression tree -> infix expression
-		fprintf( stdout, "\nInfix expression : ");
+		fprintf(stdout, "\nInfix expression : ");
 		//traverseTree( tree);
-		
+
 		// print tree with right-to-left infix traversal
-		fprintf( stdout, "\n\nTree representation:\n");
+		fprintf(stdout, "\n\nTree representation:\n");
 		//printTree(tree);
-		
+
 		// evaluate postfix expression
 		//float val = evalPostfix( expr);
 		//fprintf( stdout, "\nValue = %f\n", val);
-		
+
 		// destroy tree
 		//destroyTree( tree);
-		
-		fprintf( stdout, "\nInput an expression (postfix): ");
+
+		fprintf(stdout, "\nInput an expression (postfix): ");
 	}
 	return 0;
 }
